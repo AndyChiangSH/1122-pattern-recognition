939b1f61586d4fcd428f1f29f3d87c380f9d67d5
AndyChiangSH <andy10801@gmail.com>
2024-04-18 17:06:07 +0800

    HW2: init

---
+++ Homework/HW2/code/main.py
@@ -0,0 +1,124 @@
+import typing as t
+
+import numpy as np
+import numpy.typing as npt
+import pandas as pd
+from loguru import logger
+
+
+class LogisticRegression:
+    def __init__(self, learning_rate: float = 1e-4, num_iterations: int = 100):
+        self.learning_rate = learning_rate
+        self.num_iterations = num_iterations
+        self.weights = None
+        self.intercept = None
+
+    def fit(
+        self,
+        inputs: npt.NDArray[float],
+        targets: t.Sequence[int],
+    ) -> None:
+        """
+        Implement your fitting function here.
+        The weights and intercept should be kept in self.weights and self.intercept.
+        """
+        raise NotImplementedError
+
+    def predict(
+        self,
+        inputs: npt.NDArray[float],
+    ) -> t.Tuple[t.Sequence[np.float_], t.Sequence[int]]:
+        """
+        Implement your prediction function here.
+        The return should contains
+        1. sample probabilty of being class_1
+        2. sample predicted class
+        """
+        raise NotImplementedError
+
+    def sigmoid(self, x):
+        """
+        Implement the sigmoid function.
+        """
+        raise NotImplementedError
+
+
+class FLD:
+    def __init__(self):
+        self.w = None
+        self.m0 = None
+        self.m1 = None
+        self.sw = None
+        self.sb = None
+        self.slope = None
+
+    def fit(
+        self,
+        inputs: npt.NDArray[float],
+        targets: t.Sequence[int],
+    ) -> None:
+        raise NotImplementedError
+
+    def predict(
+        self,
+        inputs: npt.NDArray[float],
+    ) -> t.Sequence[t.Union[int, bool]]:
+        raise NotImplementedError
+
+    def plot_projection(self, inputs: npt.NDArray[float]):
+        raise NotImplementedError
+
+
+def compute_auc(y_trues, y_preds) -> float:
+    raise NotImplementedError
+
+
+def accuracy_score(y_trues, y_preds) -> float:
+    raise NotImplementedError
+
+
+def main():
+    # Read data
+    train_df = pd.read_csv('./train.csv')
+    test_df = pd.read_csv('./test.csv')
+
+    # Part1: Logistic Regression
+    x_train = train_df.drop(['target'], axis=1).to_numpy()  # (n_samples, n_features)
+    y_train = train_df['target'].to_numpy()  # (n_samples, )
+    print(y_train.shape)
+
+    x_test = test_df.drop(['target'], axis=1).to_numpy()
+    y_test = test_df['target'].to_numpy()
+
+    LR = LogisticRegression(
+        learning_rate=1e-2,  # You can modify the parameters as you want
+        num_iterations=1000,  # You can modify the parameters as you want
+    )
+    LR.fit(x_train, y_train)
+    y_pred_probs, y_pred_classes = LR.predict(x_test)
+    accuracy = accuracy_score(y_test, y_pred_classes)
+    auc_score = compute_auc(y_test, y_pred_probs)
+    logger.info(f'LR: Weights: {LR.weights[:5]}, Intercep: {LR.intercept}')
+    logger.info(f'LR: Accuracy={accuracy:.4f}, AUC={auc_score:.4f}')
+
+    # Part2: FLD
+    cols = ['27', '30']  # Dont modify
+    x_train = train_df[cols].to_numpy()
+    y_train = train_df['target'].to_numpy()
+    x_test = test_df[cols].to_numpy()
+    y_test = test_df['target'].to_numpy()
+
+    FLD_ = FLD()
+    FLD_.fit(x_train, y_train)
+    y_preds = FLD_.predict(x_test)
+    accuracy = accuracy_score(y_test, y_preds)
+    logger.info(f'FLD: m0={FLD_.m0}, m1={FLD_.m1}')
+    logger.info(f'FLD: \nSw=\n{FLD_.sw}')
+    logger.info(f'FLD: \nSb=\n{FLD_.sb}')
+    logger.info(f'FLD: \nw=\n{FLD_.w}')
+    logger.info(f'FLD: Accuracy={accuracy:.4f}')
+    FLD_.plot_projection(x_test)
+
+
+if __name__ == '__main__':
+    main()
