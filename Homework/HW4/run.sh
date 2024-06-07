948d2f6c36ac8d39c978a24890b21854d1eaefcf
AndyChiangSH <andy10801@gmail.com>
2024-06-07 11:16:08 +0800

    HW4: train ResNet-1~3

---
+++ Homework/HW4/run.sh
@@ -0,0 +1,3 @@
+python ./code/train.py --name="ResNet-1" --model="ResNet" --epochs=10 --learning_rate=0.00001
+python ./code/train.py --name="ResNet-2" --model="ResNet" --epochs=50 --learning_rate=0.000005
+python ./code/train.py --name="ResNet-3" --model="ResNet" --epochs=100 --learning_rate=0.000001
