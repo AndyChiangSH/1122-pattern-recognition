1722d905cf13686e6443f994897334c8c70fbb31
AndyChiangSH <andy10801@gmail.com>
2024-06-07 12:00:10 +0800

    HW4: train ResNet-5

--- Homework/HW4/code/dataset.py
+++ Homework/HW4/code/dataset.py
@@ -5,21 +5,6 @@ import numpy as np
 
 
 class BagDataset(Dataset):
-    def __init__(self, bags, labels):
-        self.bags = bags
-        self.labels = labels
-
-    def __len__(self):
-        return len(self.bags)
-
-    def __getitem__(self, idx):
-        bag = self.bags[idx] / 255.0
-        label = int(self.labels[idx])
-        # Reshape to [channels, depth, height, width]
-        return torch.tensor(bag, dtype=torch.float32).permute(0, 3, 1, 2), torch.tensor(label, dtype=torch.long)
-
-
-class BagDataset(Dataset):
     def __init__(self, bags, labels, transform=None):
         self.bags = bags
         self.labels = labels
